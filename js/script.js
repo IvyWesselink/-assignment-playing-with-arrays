c6a840d5915afada93f3c32214768e8e37a99047
IvyWesselink <55988292+IvyWesselink@users.noreply.github.com>
2020-03-19 22:05:00 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let array1 = [300,20,60,100,400,130,270,290,30,450,50];
+array1.sort(function(a, b){return a-b});
+array1.pop();
+array1.shift();
+
+console.log("array1 is nu:" + array1);
+
+let array2 = [1,2,3,4,5,6,7,8,9,10];
+
+let array3 = array2.map((val, index, arr) => {
+    return val * 4;
+});
+
+console.log("array3 is nu:" + array3);
